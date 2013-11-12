d7cb5fdc61bbacf459f5daff56d0788fcf869ce1
Alan Dixon <alan.g.dixon@gmail.com>
2013-11-12 18:16:47 -0500

    improved admin page with searchability and warning if the cron has not run in the last 24 hours

--- CRM/iATS/Page/iATSAdmin.php
+++ CRM/iATS/Page/iATSAdmin.php
@@ -7,19 +7,46 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_iATS_Page_iATSAdmin extends CRM_Core_Page {
   function run() {
-    // Example: Assign a variable for use in a template
+    // the current time
     $this->assign('currentTime', date('Y-m-d H:i:s'));
+    $this->assign('jobLastRunWarning', '0');
+    // check if I've got any recurring contributions setup. In theory I should only worry about iATS, but it's a problem regardless ..
+    $params = array('version' => 3, 'sequential' => 1);
+    $result = civicrm_api('ContributionRecur','getcount', $params);
+    if (!empty($result)) {
+      $this->assign('jobLastRunWarning', '1');
+      $params['api_action'] = 'IatsRecurringContributions';
+      $job = civicrm_api('Job','getSingle',$params);
+      $last_run = isset($job['last_run']) ? strtotime($job['last_run']) : '';
+      $this->assign('jobLastRun', $job['last_run']);
+      $this->assign('jobOverdue', '');
+      $overdueHours  = (time() - $last_run) / (60 * 60);
+      if (24 < $overdueHours) {
+        $this->assign('jobOverdue', $overdueHours);
+      }
+    }
     // Load the most recent requests and responses from the log files
-    $log = $this->getLog();
+    foreach(array('cc','auth_result') as $key) {
+      $search[$key] = empty($_GET['search_'.$key]) ? '' : filter_var($_GET['search_'.$key],FILTER_SANITIZE_STRING);
+    }
+    $log = $this->getLog($search);
     // $log[] = array('cc' => 'test', 'ip' => 'whatever', 'auth_result' => 'blah');
     $this->assign('iATSLog', $log);
+    $this->assign('search', $search);
     parent::run();
   }
 
-  function getLog($n = 10) {
+  function getLog($search = array(), $n = 40) {
     // avoid sql injection attacks
     $n = (int) $n;
-    $sql = "SELECT * FROM civicrm_iats_request_log request LEFT JOIN civicrm_iats_response_log response ON request.invoice_num = response.invoice_num ORDER BY request.id DESC LIMIT $n";
+    $filter = array();
+    foreach($search as $key => $value) {
+      if (!empty($value)) {
+        $filter[] = "$key RLIKE '$value'";
+      }
+    }
+    $where = empty($filter) ?  '' : " WHERE ".implode(' AND ',$filter);
+    $sql = "SELECT * FROM civicrm_iats_request_log request LEFT JOIN civicrm_iats_response_log response ON request.invoice_num = response.invoice_num $where ORDER BY request.id DESC LIMIT $n";
     $dao = CRM_Core_DAO::executeQuery($sql);
     $log = array();
     $params = array('version' => 3, 'sequential' => 1, 'return' => 'contribution_id');
--- templates/CRM/iATS/Page/iATSAdmin.tpl
+++ templates/CRM/iATS/Page/iATSAdmin.tpl
@@ -1,8 +1,21 @@
-<h3>Additional iATS-specific recent transaction information.</h3>
-
+{if $jobLastRunWarning == 1}
+<h3>{if $jobOverdue != ''}{ts}Warning!{/ts}{else}{ts}Cron Running{/ts}{/if}</h3>
 <p>The current time is {$currentTime}</p>
+<p>{ts 1=$jobLastRun}Your iATS Payments cron last ran at %1.{/ts}</p>
+<p>{if $jobOverdue != ''}<strong style="font-size: 120%">{ts}Your recurring contributions for iATS Payments requires a correctly setup and functioning cron job that runs daily. You need to take action now.{/ts}</strong>{else}{ts}It's all good.{/ts}{/if}</p>
+{/if}
 
+<h3>Recent transations using iATS Payments</h3>
+<form method="GET">
+<fieldset><legend>Filter results</legend>
+<div><em>Filter your results by any part of the last 4 digits of a credit card or the authorization string</em></div> 
+CC <input size="20" type="text" name="search_cc" value="{$search.cc}">
+Auth <input size="20" type="text" name="search_auth_result" value="{$search.auth_result}">
+<input type="submit">
+</fieldset>
+</form>
 <table class="iats-report">
+<caption>Recent transactions with the IATS Payment Processor</caption>
 <tr>
   <th>{ts}Invoice Number{/ts}</th>
   <th>{ts}IP{/ts}</th>
