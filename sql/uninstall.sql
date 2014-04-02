1f75797c6e0d9b46d9f30f8c0d99fb0f486759d7
adixon <alan.g.dixon@gmail.com>
2014-04-02 16:23:52 -0400

    added missing drop table for the iats_verify table

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS `civicrm_iats_customer_codes`;
 DROP TABLE IF EXISTS `civicrm_iats_request_log`;
 DROP TABLE IF EXISTS `civicrm_iats_response_log`;
+DROP TABLE IF EXISTS `civicrm_iats_verify`;
