305641edbe5f5e12543c673fb49896aa8e53e730
Karin Gerritsen <karin@semper-it.com>
2015-01-12 18:23:01 -0700

    Fixed typo

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -2,4 +2,4 @@ DROP TABLE IF EXISTS `civicrm_iats_customer_codes`;
 DROP TABLE IF EXISTS `civicrm_iats_request_log`;
 DROP TABLE IF EXISTS `civicrm_iats_response_log`;
 DROP TABLE IF EXISTS `civicrm_iats_verify`;
-DROP TABLE IF EXISTS `civicrm_iats_dduk_validate`;
+DROP TABLE IF EXISTS `civicrm_iats_ukdd_validate`;
