97e440dab5a0f40b1e84214f1f15aa486e742939
adixon <alan.g.dixon@gmail.com>
2015-06-05 12:15:42 -0400

    fixed signature of singleton

--- CRM/Core/Payment/iATSServiceUKDD.php
+++ CRM/Core/Payment/iATSServiceUKDD.php
@@ -49,7 +49,7 @@ class CRM_Core_Payment_iATSServiceUKDD extends CRM_Core_Payment {
     $this->_profile['iats_domain'] = parse_url($this->_paymentProcessor['url_site'], PHP_URL_HOST);
   }
 
-  static function &singleton($mode, &$paymentProcessor) {
+  static function &singleton($mode, &$paymentProcessor, &$paymentForm = NULL, $force = FALSE) {
     $processorName = $paymentProcessor['name'];
     if (self::$_singleton[$processorName] === NULL) {
       self::$_singleton[$processorName] = new CRM_Core_Payment_iATSServiceUKDD($mode, $paymentProcessor);
