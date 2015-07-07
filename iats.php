01756e7bab1469244651f06a8bc93acc1d2020cd
Karin Gerritsen <karin@semper-it.com>
2015-07-07 10:40:17 -0600

    roll back

--- iats.php
+++ iats.php
@@ -372,7 +372,6 @@ function iats_civicrm_pre($op, $objectName, $objectId, &$params) {
           }
           break;
         case 'iATSServiceContributionRecur': // cc recurring contribution record
-          break;
         case 'iATSServiceSWIPEContributionRecur':
           // we've already taken the first payment, so calculate the next one
           $params['contribution_status_id'] = 5;
