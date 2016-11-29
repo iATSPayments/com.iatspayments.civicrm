37b66ffce0486091c88eb1764cb4fbe61f69cb31
David Reedy Jr <davidrjr.konadave@gmail.com>
2016-11-29 09:48:10 -0600

    Add support for mid as well as crid in the URL

--- iats.php
+++ iats.php
@@ -1212,9 +1212,26 @@ function iats_civicrm_buildForm_CRM_Contribute_Form_UpdateSubscription(&$form) {
 }
 
 function iats_civicrm_buildForm_CRM_Contribute_Form_UpdateBilling(&$form) {
-
-  $form->addElement('hidden', 'crid', $_GET['crid']);
-
+  // add hidden form field for the contribution recur ID taken from URL
+  // if not specified directly, look it up via a membership ID
+  $crid = CRM_Utils_Array::value('crid', $_GET);
+  if (!$crid) {
+    $mid = CRM_Utils_Array::value('mid', $_GET);
+    if ($mid) {
+      try {
+        $crid = civicrm_api3('Membership', 'getvalue', array(
+          'id' => $mid,
+          'return' => 'contribution_recur_id',
+        ));
+      }
+      catch (CiviCRM_API3_Exception $e) {
+        $crid = 0;
+      }
+    }
+  }
+  if ($crid) {
+    $form->addElement('hidden', 'crid', $crid);
+  }
 }
 
 /**
