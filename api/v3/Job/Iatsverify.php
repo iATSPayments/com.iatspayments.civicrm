93fcfb7ffee710b93ff75b533b53f6d5f3f3937a
adixon <alan.g.dixon@gmail.com>
2017-05-19 12:22:39 -0400

    converted separate verify jobs into a single better one

--- api/v3/Job/Iatsverify.php
+++ api/v3/Job/Iatsverify.php
@@ -2,11 +2,11 @@
 
 /**
  * @file
- * Contains the ACH/EFT Verification API Job.
+ * Contains the IATS Payments Verification API Job.
  */
 
 /**
- * Job.IatsACHEFTVerify API specification (optional).
+ * Job.IatsVerify API specification
  * This is used for documentation and validation.
  *
  * @param array $spec description of fields supported by this API call
@@ -15,12 +15,41 @@
  *
  * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
  */
-function _civicrm_api3_job_iatsacheftverify_spec(&$spec) {
-  // Todo - call this job with optional parameters?
+function _civicrm_api3_job_iatsverify_spec(&$spec) {
+  $spec['recur_id'] = array(
+    'name' => 'recur_id',
+    'title' => 'Recurring payment id',
+    'api.required' => 0,
+    'type' => 1,
+  );
+  $spec['contribution_id'] = array(
+    'name' => 'contribution_id',
+    'title' => 'Test a single contribution by CiviCRM contribution table id.',
+    'api.required' => 0,
+    'type' => 1,
+  );
+  $spec['invoice_id'] = array(
+    'name' => 'invoice_id',
+    'title' => 'Test a single contribution by invoice id.',
+    'api.required' => 0,
+    'type' => 1,
+  );
+  $spec['payment_instrument_id'] = array(
+    'name' => 'payment_instrument_id',
+    'title' => 'Test contributions by payment method.',
+    'api.required' => 0,
+    'type' => 1,
+  );
+  $spec['reverify'] = array(
+    'name' => 'reverify',
+    'title' => 'Reverify contributions',
+    'api.required' => 0,
+    'type' => 1,
+  );
 }
 
 /**
- * Job.IatsACHEFTVerify API.
+ * Job.IatsVerify API.
  *
  * @param array $params
  *
@@ -30,39 +59,60 @@ function _civicrm_api3_job_iatsacheftverify_spec(&$spec) {
  * @see civicrm_api3_create_error
  *
  * @throws API_Exception
- *  * Look up all pending (status = 2) ACH/EFT contributions and see if they've been approved or rejected
+ *  * Look up all incomplete or pending (status = 2) contributions and see if they've been received approved or rejected payments 
+ * at iATS, looked up via the Journal
  * Update the corresponding recurring contribution record to status = 1 (or 4)
  * This works for both the initial contribution and subsequent contributions of recurring contributions, as well as one offs.
  * TODO: what kind of alerts should be provided if it fails?
  *
  */
-function civicrm_api3_job_iatsacheftverify($iats_service_params) {
+function civicrm_api3_job_iatsverify($params) {
 
   $settings = CRM_Core_BAO_Setting::getItem('iATS Payments Extension', 'iats_settings');
   $receipt_recurring = $settings['receipt_recurring'];
   define('IATS_VERIFY_DAYS', 30);
   // I've added an extra 2 days when getting candidates from CiviCRM to be sure i've got them all.
-  $civicrm_verify_days = IATS_VERIFY_DAYS + 2;
-  // Get all the pending direct debit contributions that still need approval within the last civicrm_verify_days.
-  // And see if they are approved in my iATS Journal
+  $verify_days = IATS_VERIFY_DAYS + 2;
+  // Get all the contributions that may need approval within the last verify_days.
+  // And see if they are approved in my iATS Journal.
+  // This could include ACH/EFT approvals, as well as CC contributions that were completed but didn't get back from iATS.
   // Count the number of each kind found.
   $processed = array(1 => 0, 4 => 0);
   // Save all my api error result messages.
   $error_log = array();
+  //
+  $select_params = array(
+    'sequential' => 1,
+    'receive_date' => array('>' => "now - $verify_days day"),
+    'options' => array('limit' => 0),
+    'contribution_status_id' => array('NOT IN' => array('Completed', 'Failed')),
+    'invoice_id' => array('IS NOT NULL' => 1),
+    'contribution_test' => 0,
+    'return' => array('trxn_id', 'invoice_id', 'contribution_recur_id', 'contact_id', 'source'),
+  );
+  // get my parameters
+  $recur_id = empty($params['recur_id']) ? 0 : ((int) $params['recur_id']);
+  unset($params['recur_id']);
+  if ($recur_id) {
+    $select_params['contribution_recur_id'] = $recur_id;
+  }
+  $contribution_id = empty($params['contribution_id']) ? 0 : ((int) $params['contribution_id']);
+  unset($params['contribution_id']);
+  if ($contribution_id) {
+    $select_params['contribution_id'] = $contribution_id;
+  }
+  $invoice_id = empty($params['invoice_id']) ? '' : trim($params['invoice_id']);
+  unset($params['invoice_id']);
+  if ($invoice_id) {
+    $select_params['invoice_id'] = $invoice_id;
+  }
+
+  $message = '';
   try {
-    $acheft_pending = civicrm_api3('Contribution', 'get', array(
-      'sequential' => 1,
-      'receive_date' => array('>' => "now - $civicrm_verify_days day"),
-      'options' => array('limit' => 0),
-      'contribution_status_id' => "Pending",
-      'invoice_id' => array('IS NOT NULL' => 1),
-      'contribution_test' => 0,
-      'payment_instrument_id' => 'Debit Card',
-      'return' => array('trxn_id', 'invoice_id', 'contribution_recur_id', 'contact_id', 'source'),
-    ));
-    $message = '<br />' . ts('Found %1 pending ACH/EFT contributions to verify.', array(1 => count($acheft_pending['values'])));
-    // CRM_Core_Error::debug_var('Pending ACHEFT', $acheft_pending);
-    foreach ($acheft_pending['values'] as $contribution) {
+    $contributions_verify = civicrm_api3('Contribution', 'get', $select_params);
+    $message .= '<br />' . ts('Found %1 contributions to verify.', array(1 => count($contributions_verify['values'])));
+    // CRM_Core_Error::debug_var('Verifying contributions', $contributions_verify);
+    foreach ($contributions_verify['values'] as $contribution) {
       $journal_matches = civicrm_api3('IatsPayments', 'get_journal', array(
         'sequential' => 1,
         'inv' => $contribution['invoice_id'],
@@ -126,16 +176,17 @@ function civicrm_api3_job_iatsacheftverify($iats_service_params) {
           2 => array($contribution['contact_id'], 'Integer'),
           3 => array($contribution['id'], 'Integer'),
           4 => array($contribution_status_id, 'Integer'),
-          5 => array($contribution['contribution_recur_id'], 'Integer'),
+          5 => array($journal_entry['rst'], 'String'),
+          6 => array($contribution['contribution_recur_id'], 'Integer'),
         );
         if (empty($contribution['contribution_recur_id'])) {
-          unset($query_params[5]);
+          unset($query_params[6]);
           CRM_Core_DAO::executeQuery("INSERT INTO civicrm_iats_verify
-            (customer_code, cid, contribution_id, contribution_status_id, verify_datetime) VALUES (%1, %2, %3, %4, NOW())", $query_params);
+            (customer_code, cid, contribution_id, contribution_status_id, verify_datetime, auth_result) VALUES (%1, %2, %3, %4, NOW(), %5)", $query_params);
         }
         else {
           CRM_Core_DAO::executeQuery("INSERT INTO civicrm_iats_verify
-            (customer_code, cid, contribution_id, contribution_status_id, verify_datetime, recur_id) VALUES (%1, %2, %3, %4, NOW(), %5)", $query_params);
+            (customer_code, cid, contribution_id, contribution_status_id, verify_datetime, recur_id) VALUES (%1, %2, %3, %4, NOW(), %5, %6)", $query_params);
         }
       }
     }
