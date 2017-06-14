7d6ab2bdafa739ad15654ba5ec5f81b75263ba34
adixon <alan.g.dixon@gmail.com>
2017-06-14 15:47:10 -0400

    fixed sql for upgrade 1-6-001

--- sql/upgrade_1_6_001.sql
+++ sql/upgrade_1_6_001.sql
@@ -1,1 +1,2 @@
-ALTER TABLE civicrm_iats_verify ADD COLUMN `auth_result` varchar(255) COMMENT 'Authorization string from iATS' ADD INDEX (auth_result)
+ALTER TABLE civicrm_iats_verify ADD COLUMN `auth_result` varchar(255) COMMENT 'Authorization string from iATS';
+ALTER TABLE civicrm_iats_verify ADD INDEX (auth_result);
