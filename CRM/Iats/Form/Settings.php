7d1714415bd46987ea8ed33088feb83ae26a76aa
adixon <alan.g.dixon@gmail.com>
2019-06-04 11:42:40 -0400

    Modify file name of settings form

--- CRM/Iats/Form/Settings.php
+++ CRM/Iats/Form/Settings.php
@@ -1,61 +1,67 @@
 <?php
 
-use CRM_Iats_ExtensionUtil as E;
+/**
+ * @file
+ */
+
+require_once 'CRM/Core/Form.php';
 
 /**
- * Form controller class
+ * Form controller class.
  *
- * @see https://wiki.civicrm.org/confluence/display/CRMDOC/QuickForm+Reference
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
  */
 class CRM_Iats_Form_Settings extends CRM_Core_Form {
+
+  /**
+   *
+   */
   public function buildQuickForm() {
 
     // Add form elements.
     $this->add(
+    // Field type.
       'text',
+    // Field name.
       'email_recurring_failure_report',
       ts('Email Recurring Contribution failure reports to this Email address')
     );
     $this->addRule('email_recurring_failure_report', ts('Email address is not a valid format.'), 'email');
     $this->add(
+    // Field type.
       'text',
+    // Field name.
       'recurring_failure_threshhold',
       ts('When failure count is equal to or greater than this number, push the next scheduled contribution date forward')
     );
     $this->addRule('recurring_failure_threshhold', ts('Threshhold must be a positive integer.'), 'integer');
     $receipt_recurring_options =  array('0' => 'Never', '1' => 'Always', '2' => 'As set for a specific Contribution Series');
     $this->add(
+    // Field type.
       'select',
+    // Field name.
       'receipt_recurring',
       ts('Email receipt for a Contribution in a Recurring Series'),
       $receipt_recurring_options
     );
 
     $this->add(
+    // Field type.
       'checkbox',
-      'use_cryptogram',
-      ts('Enable use of cryptogram (experimental, not working)')
-    );
-    
-    $this->add(
-      'text',
-      'ach_category_text',
-      ts('ACH Category Text')
-    );
-
-    $this->add(
-      'checkbox',
+    // Field name.
       'no_edit_extra',
       ts('Disable extra edit fields for Recurring Contributions')
     );
 
     $this->add(
+    // Field type.
       'checkbox',
+    // Field name.
       'enable_update_subscription_billing_info',
       ts('Enable self-service updates to recurring contribution Contact Billing Info.')
     );
 
-    /* These checkboxes are not yet implemented, ignore for now
+    /* These checkboxes are not yet implemented, ignore for now 
     $this->add(
       'checkbox', // field type
       'import_quick', // field name
@@ -67,31 +73,72 @@ class CRM_Iats_Form_Settings extends CRM_Core_Form {
       'import_recur', // field name
       ts('Import recurring iATS transactions into CiviCRM for known series (e.g. "iATS managed recurring").')
     );
-   */
 
-    $result = CRM_Core_BAO_Setting::getItem('iATS FAPS Payments Extension', 'faps_settings');
-    $defaults = (empty($result)) ? array() : $result;
-    if (empty($defaults['recurring_failure_threshhold'])) {
-      $defaults['recurring_failure_threshhold'] = 3;
-    }
-    if (empty($defaults['ach_category_text'])) {
-      $defaults['ach_category_text'] = FAPS_DEFAULT_ACH_CATEGORY_TEXT;
-    }
-    $this->setDefaults($defaults);
+    $this->add(
+      'checkbox', // field type
+      'import_series', // field name
+      ts('Allow creation of new recurring series from iATS imports. (e.g. "mobile recurring")')
+    );
+    */
+    
+    $this->add(
+      'checkbox',
+      'enable_public_future_recurring_start',
+      ts('Enable public selection of future recurring start dates.')
+    );
 
+    $days = array('-1' => 'disabled');
+    for ($i = 1; $i <= 28; $i++) {
+      $days["$i"] = "$i";
+    }
+    $attr = array(
+      'size' => 29,
+      'style' => 'width:150px',
+      'required' => FALSE,
+    );
+    $day_select = $this->add(
+    // Field type.
+      'select',
+    // Field name.
+      'days',
+      ts('Restrict allowable days of the month for Recurring Contributions'),
+      $days,
+      FALSE,
+      $attr
+    );
+
+    $day_select->setMultiple(TRUE);
+    $day_select->setSize(29);
     $this->addButtons(array(
       array(
         'type' => 'submit',
-        'name' => E::ts('Submit'),
+        'name' => ts('Submit'),
         'isDefault' => TRUE,
       ),
     ));
 
-    // export form elements
+    $result = CRM_Core_BAO_Setting::getItem('iATS Payments Extension', 'iats_settings');
+    $defaults = (empty($result)) ? array() : $result;
+    if (empty($defaults['recurring_failure_threshhold'])) {
+      $defaults['recurring_failure_threshhold'] = 3;
+    }
+    $this->setDefaults($defaults);
+    $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => ts('Submit'),
+        'isDefault' => TRUE,
+      ),
+    ));
+
+    // Export form elements.
     $this->assign('elementNames', $this->getRenderableElementNames());
     parent::buildQuickForm();
   }
 
+  /**
+   *
+   */
   public function postProcess() {
     $values = $this->exportValues();
     foreach (array('qfKey', '_qf_default', '_qf_Settings_submit', 'entryURL') as $key) {
@@ -99,7 +146,7 @@ class CRM_Iats_Form_Settings extends CRM_Core_Form {
         unset($values[$key]);
       }
     }
-    CRM_Core_BAO_Setting::setItem($values, 'iATS FAPS Payments Extension', 'faps_settings');
+    CRM_Core_BAO_Setting::setItem($values, 'iATS Payments Extension', 'iats_settings');
     parent::postProcess();
   }
 
@@ -115,7 +162,6 @@ class CRM_Iats_Form_Settings extends CRM_Core_Form {
     // the 'label'.
     $elementNames = array();
     foreach ($this->_elements as $element) {
-      /** @var HTML_QuickForm_Element $element */
       $label = $element->getLabel();
       if (!empty($label)) {
         $elementNames[] = $element->getName();
