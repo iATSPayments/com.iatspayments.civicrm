45eae21894e5b50577c92f78706298c3abc8a972
adixon <alan.g.dixon@gmail.com>
2019-07-02 10:42:42 -0400

    For FAPS, Set the payment status based on the authResponse value. Also update since we always have a transactionId

--- api/v3/FapsTransaction/Journal.php
+++ api/v3/FapsTransaction/Journal.php
@@ -34,19 +34,34 @@ function civicrm_api3_faps_transaction_journal($params) {
   // return civicrm_api3_create_success(TRUE, array('test' => TRUE));
   try {
     $data = $params['orderInfo'];
-    // There is a unique keys on transactionId
-    // If I don't have one don't overwrite.
-    if (empty($params['transactionId'])) {
-      $transactionId = 'NULL';
-      $sql_action = 'INSERT IGNORE ';
-    }
-    else {
-      $transactionId = (int) $params['transactionId'];
-      $sql_action = 'REPLACE INTO ';
-    }
+    $transactionId = (int) $params['transactionId'];
+    $sql_action = 'REPLACE INTO ';
     $cardType = isset($params['ccInfo']['cardType']) ? $params['ccInfo']['cardType'] : '';
     $isAch = empty($params['isAch']) ? 0 : 1;
-    $status_id = $data['isSuccessful'] ? ($data['authResponse'] == 'Pending' ? 2 : 1) : 4; // 1 = complete, 2 = pending, 4 = fail
+    $status_id = 4; // default fail?
+    // calculate the status id of the payment based on the authResponse, which
+    // is different for ACH vs CC
+    if ($data['isSuccessful']) {
+      if ($isAch) {
+        switch ($data['authResponse']) {
+          case 'Settled': $status_id = 1; break;
+          case 'Pending': $status_id = 2; break;
+          default: $status_id = 4; break; // fail
+        }
+      }
+      else { // cc responses are different
+        if ("Approved ".$data['authCode'] == $data['authResponse']) {
+          $status_id = 1; 
+        }
+        else {
+          switch($data['authResponse']) {
+            case 'COMPLETED': $status_id = 1; break;
+            case 'Unknown': $status_id = 2; break;
+            default: $status_id = 4; break; // fail
+          }
+        }
+      }
+    }
     $query_params = array(
       2 => array($data['authCode'], 'String'),
       3 => array($isAch, 'Integer'),
