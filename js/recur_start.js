0f888922ea89b1e81c2d05f7a90388bf740e3d4d
adixon <alan.g.dixon@gmail.com>
2019-11-13 15:31:45 -0500

    Fix recur_start to work for admin pages by hiding injected field + style cleanups cj -> $

--- js/recur_start.js
+++ js/recur_start.js
@@ -6,20 +6,25 @@
 /*global CRM, ts */
 cj(function ($) {
   'use strict';
-   $('.is_recur-section #iats-recurring-start-date').remove();
-   $('.is_recur-section').append($('#iats-recurring-start-date'));
-   cj('input[id="is_recur"]').on('change', function() {
+   if ($('.is_recur-section').length) {
+     $('.is_recur-section #iats-recurring-start-date').remove();
+     $('.is_recur-section').append($('#iats-recurring-start-date'));
+     cj('input[id="is_recur"]').on('change', function() {
+       toggleRecur();
+     });
      toggleRecur();
-   });
-   toggleRecur();
+   }
+   else { // I'm not on the right kind of page, just remove the extra field
+     $('#iats-recurring-start-date').remove();
+   }
 
    function toggleRecur( ) {
-     var isRecur = cj('input[id="is_recur"]:checked');
+     var isRecur = $('input[id="is_recur"]:checked');
      if (isRecur.val() > 0) {
-       cj('#iats-recurring-start-date').show().val('');
+       $('#iats-recurring-start-date').show().val('');
      }
      else {
-       cj('#iats-recurring-start-date').hide();
+       $('#iats-recurring-start-date').hide();
        $("#iats-recurring-start-date option:selected").prop("selected", false);
        $("#iats-recurring-start-date option:first").prop("selected", "selected");
      }
