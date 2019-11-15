065cf3da2a8b60b5ee81ee46118381191230533e
adixon <alan.g.dixon@gmail.com>
2019-11-15 15:51:06 -0500

    Exclude non-active processors in recurring job, and handle sites with a lot of iATS payment processors enabled

--- iats.php
+++ iats.php
@@ -566,7 +566,11 @@ function _iats_civicrm_is_iats($payment_processor_id) {
 function _iats_filter_payment_processors($class, $processors = array(), $params = array()) {
   $list = array();
   $params['class_name'] = ['LIKE' => 'Payment_' . $class];
-
+  // On the chance that there are a lot of payment processors and the caller
+  // hasn't specified a limit, assume they want them all.
+  if (empty($params['options']['limit'])) {
+    $params['options']['limit'] = 0;
+  }
   // Set the domain id if not passed in.
   if (!array_key_exists('domain_id', $params)) {
     $params['domain_id']    = CRM_Core_Config::domainID();
