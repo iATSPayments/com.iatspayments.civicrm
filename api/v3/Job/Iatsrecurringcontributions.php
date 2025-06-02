f4e479640952d515b07b3b387578a2259e78b7e2
Alan Dixon <alan.g.dixon@gmail.com>
2025-06-02 14:45:57 -0400

    get a label for the contribution status of the payment, add to subject line of activity

--- api/v3/Job/Iatsrecurringcontributions.php
+++ api/v3/Job/Iatsrecurringcontributions.php
@@ -312,6 +312,7 @@ function civicrm_api3_job_Iatsrecurringcontributions($params) {
         }
       }
       $output[] = $result['message'];
+      $payment_status_label = CRM_Core_PseudoConstant::getLabel('CRM_Contribute_BAO_Contribution', 'contribution_status_id', $result['payment_status_id']);
       $result = civicrm_api('activity', 'create',
         array(
           'version'       => 3,
@@ -319,7 +320,7 @@ function civicrm_api3_job_Iatsrecurringcontributions($params) {
           'source_contact_id'   => $contact_id,
           'source_record_id' => $contribution['id'],
           'assignee_contact_id' => $contact_id,
-          'subject'       => ts('Attempted iATS Payments (%1) Recurring Contribution for %2', array(1 => $paymentClass, 2 => $total_amount)),
+          'subject'       => ts('Attempted iATS Payments (%1) Recurring Contribution for %2, result %3', array(1 => $paymentClass, 2 => $total_amount, 3 => $payment_status_label)),
           'status_id'       => 2,
           'activity_date_time'  => date("YmdHis"),
         )
